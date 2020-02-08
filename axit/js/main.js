f837673318f9aae421d919e367bf0f6ef40b3e15
Yaroslav <dombrovskiy91@gmail.com>
2020-02-08 20:52:02 +0200

    desctop+tablet+mobile

--- axit/js/main.js
+++ axit/js/main.js
@@ -1,14 +1,18 @@
 $(function(){
-  $('.header-slider').slick({
-    arrows:false,
-    dots:true,
-    dotsClass:'header-dots'
-  });
-
-
   $('.menu__btn').on('click', function(){
     $('.menu__list').slideToggle();
   });
-
-
 });
+
+$(function() {
+  $(window).scroll(function() {
+  if($(this).scrollTop() != 0) {
+  $('#toTop').fadeIn();
+  } else {
+  $('#toTop').fadeOut();
+  }
+  });
+  $('#toTop').click(function() {
+  $('body,html').animate({scrollTop:0},800);
+  });
+  });
